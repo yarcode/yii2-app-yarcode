721c364eb44205c54b7f38f2398e355146921c2b
Alexey <alexey.samoylov@gmail.com>
2017-07-05 18:21:20 +0700

    added dotenv to the environments

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -3,3 +3,11 @@ Yii::setAlias('@common', dirname(__DIR__));
 Yii::setAlias('@frontend', dirname(dirname(__DIR__)) . '/frontend');
 Yii::setAlias('@backend', dirname(dirname(__DIR__)) . '/backend');
 Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
+
+Yii::$container->setSingleton(\Dotenv\Dotenv::class, function() {
+    $env = new Dotenv\Dotenv(dirname(dirname(__DIR__)));
+    $env->load();
+    return $env;
+});
+
+Yii::$container->get(\Dotenv\Dotenv::class);

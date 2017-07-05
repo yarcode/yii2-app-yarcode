63ebf5f99f5443b6f6493109f8492262ccc3f013
Alexey <alexey.samoylov@gmail.com>
2017-07-05 16:57:07 +0700

    added ide helper file

---
+++ tools/ide.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Yii bootstrap file.
+ * Used for enhanced IDE code autocompletion.
+ */
+class Yii extends \yii\BaseYii
+{
+    /**
+     * @var BaseApplication|WebApplication|ConsoleApplication the application instance
+     */
+    public static $app;
+}
+
+/**
+ * Class BaseApplication
+ * Used for properties that are identical for both WebApplication and ConsoleApplication
+ *
+ * @property \yii\rbac\DbManager $authManager
+ * @property \common\kernel\Kernel $kernel
+ * @property User $user
+ */
+abstract class BaseApplication extends yii\base\Application
+{
+}
+
+/**
+ * Class WebApplication
+ * Include only Web application related components here
+ */
+class WebApplication extends yii\web\Application
+{
+}
+
+/**
+ * Class ConsoleApplication
+ * Include only Console application related components here
+ */
+class ConsoleApplication extends yii\console\Application
+{
+}
+
+/**
+ * Class User
+ *
+ * @property \common\models\UserAccount $identity
+ */
+class User extends yii\web\User
+{
+}

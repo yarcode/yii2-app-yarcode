3fde855a114239a5a8a8d1201193a698604b215d
Oleg <theorder83@gmail.com>
2017-07-18 20:39:50 +0700

    fix test config

---
+++ api/config/main-local.php
@@ -0,0 +1,12 @@
+<?php
+
+$config = [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => '8FHGZNI5RYMqmGLbcBV7NKLKetTnnH4h',
+        ],
+    ],
+];
+
+return $config;

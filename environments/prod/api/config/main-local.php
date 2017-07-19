9438a700093258fd054ada8f9cda157c58d37078
Oleg <theorder83@gmail.com>
2017-07-19 11:33:47 +0700

    add api config in environments

--- environments/prod/api/config/main-local.php
+++ environments/prod/api/config/main-local.php
@@ -1,12 +1,9 @@
 <?php
-
-$config = [
+return [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '8FHGZNI5RYMqmGLbcBV7NKLKetTnnH4h',
+            'cookieValidationKey' => '',
         ],
     ],
 ];
-
-return $config;
